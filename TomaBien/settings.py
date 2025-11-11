8ef8bfce91f70e763af4d387fb9f5fab70a013aa
ezguapp <164395315+ezguapp@users.noreply.github.com>
2025-11-11 00:02:43 -0300

    Segundo Commit4

--- TomaBien/settings.py
+++ TomaBien/settings.py
@@ -18,6 +18,13 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # carpeta donde se recopilarán los estáticos
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # si tienes archivos en /static/
+]
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
