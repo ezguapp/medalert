ec19c43b7063554b9d26593b7b639e183f6cd70e
ezguapp <164395315+ezguapp@users.noreply.github.com>
2025-11-19 00:11:10 -0300

    Login Corregido

--- TomaBien/settings.py
+++ TomaBien/settings.py
@@ -152,3 +152,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'        # nombre de tu URL de login
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
